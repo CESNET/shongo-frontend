4e595e435a68d40bbb34cedc1d1608c39d2696f6
Michal Drobňák <493239@mail.muni.cz>
2021-11-02 19:41:08 +0100

    feat(data table): Add basic data table

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MaterialModule } from '../material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, SharedModule],
 })
 export class HomeModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../modules/material.module';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, MaterialModule],
-  exports: [],
+  declarations: [DataTableComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
+  exports: [DataTableComponent],
 })
 export class SharedModule {}
