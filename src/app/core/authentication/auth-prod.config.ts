e55a11595e8cf2c3d2d776cd70b765f55a218be9
Michal Drobňák <michal.drobnak@papirfly.com>
2022-10-09 20:10:54 +0200

    Fix auth callback

--- src/app/core/authentication/auth-prod.config.ts
+++ src/app/core/authentication/auth-prod.config.ts
@@ -5,7 +5,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
  */
 export const authConfig: AuthConfig = {
   issuer: 'https://login.cesnet.cz/oidc/',
-  redirectUri: window.location.origin + '/oauth2callback',
+  redirectUri: window.location.origin,
   clientId: '49ae627d-7b2e-4052-8421-bcc16ee0c3d6',
   responseType: 'code',
   scope:
