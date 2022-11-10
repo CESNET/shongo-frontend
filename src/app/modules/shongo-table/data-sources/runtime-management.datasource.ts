6acf7347eb92ae260fe4b96487c313bc005de836
Michal Drobňák <michal.drobnak@papirfly.com>
2022-11-10 18:32:07 +0100

    Fix & refactor runtime management datasource

--- src/app/modules/shongo-table/data-sources/runtime-management.datasource.ts
+++ src/app/modules/shongo-table/data-sources/runtime-management.datasource.ts
@@ -11,6 +11,7 @@ import { SetMicrophoneLevelButton } from 'src/app/modules/reservation-request/cl
 import { SetVideoEnabledButton } from 'src/app/modules/reservation-request/classes/buttons/set-video-enabled.button';
 import { UserSnapshotDialogComponent } from 'src/app/modules/reservation-request/components/user-snapshot-dialog/user-snapshot-dialog.component';
 import { ApiResponse } from 'src/app/shared/models/rest-api/api-response.interface';
+import { RuntimeParticipant } from 'src/app/shared/models/rest-api/runtime-participant.interface';
 import { StringBool } from 'src/app/shared/models/types/string-bool.type';
 import { toTitleCase } from 'src/app/utils/to-title-case';
 import { CustomActionButton } from '../buttons/custom-action-button';
@@ -127,27 +128,73 @@ export class RuntimeManagementDataSource extends DataTableDataSource<RuntimePart
           const { count, items } = tableData;
 
           const mappedItems = items.map((item) => ({
-            id: String(item.id),
-            displayName: item.displayName,
-            participant:
-              (item.name ?? item.displayName) +
-              (item.email ? ` (${item.email})` : ''),
-            role: toTitleCase(item.role),
-            layout: toTitleCase(item.layout),
-            microphone:
-              `${item.microphoneEnabled ? $localize`Yes` : $localize`No`}` +
-              (item.microphoneEnabled && item.microphoneLevel
-                ? ` (${item.microphoneLevel}%)`
-                : ''),
-            microphoneLevel: String(item.microphoneLevel),
-            video: item.videoEnabled ? $localize`Yes` : $localize`No`,
-            videoSnapshot: String(item.videoSnapshot) as StringBool,
-            microphoneEnabled: String(item.microphoneEnabled) as StringBool,
-            videoEnabled: String(item.videoEnabled) as StringBool,
+            id: this._getId(item),
+            displayName: this._getDisplayName(item),
+            participant: this._getParticipant(item),
+            role: this._getRole(item),
+            layout: this._getLayout(item),
+            microphone: this._getMicrophone(item),
+            microphoneLevel: this._getMicrophoneLevel(item),
+            video: this._getVideo(item),
+            videoSnapshot: this._getVideoSnapshot(item),
+            microphoneEnabled: this._getMicrophoneEnabled(item),
+            videoEnabled: this._getVideoEnabled(item),
           }));
 
           return { count, items: mappedItems };
         })
       );
+  }
+
+  private _getId(item: RuntimeParticipant): string {
+    return item.id ? String(item.id) : $localize`None`;
+  }
+
+  private _getDisplayName(item: RuntimeParticipant): string {
+    return item.displayName ?? item.name ?? $localize`Unknown user`;
+  }
+
+  private _getParticipant(item: RuntimeParticipant): string {
+    return (
+      (item.name ?? item.displayName ?? $localize`Unknown user`) +
+      (item.email ? ` (${item.email})` : '')
+    );
+  }
+
+  private _getRole(item: RuntimeParticipant): string {
+    return item.role ? toTitleCase(item.role) : $localize`None`;
+  }
+
+  private _getLayout(item: RuntimeParticipant): string {
+    return item.layout ? toTitleCase(item.layout) : $localize`None`;
+  }
+
+  private _getMicrophone(item: RuntimeParticipant): string {
+    return (
+      `${!!item.microphoneEnabled ? $localize`Yes` : $localize`No`}` +
+      (item.microphoneEnabled && item.microphoneLevel
+        ? ` (${item.microphoneLevel}%)`
+        : '')
+    );
+  }
+
+  private _getMicrophoneLevel(item: RuntimeParticipant): string {
+    return item.microphoneLevel ? String(item.microphoneLevel) : 'Unknown';
+  }
+
+  private _getVideo(item: RuntimeParticipant): string {
+    return !!item.videoEnabled ? $localize`Yes` : $localize`No`;
+  }
+
+  private _getVideoSnapshot(item: RuntimeParticipant): StringBool {
+    return String(!!item.videoSnapshot) as StringBool;
+  }
+
+  private _getMicrophoneEnabled(item: RuntimeParticipant): StringBool {
+    return String(!!item.microphoneEnabled) as StringBool;
+  }
+
+  private _getVideoEnabled(item: RuntimeParticipant): StringBool {
+    return String(!!item.videoEnabled) as StringBool;
   }
 }
