c1d70892821d1c2d770acdf007e6f847fcb40dbc
Michal Drobňák <mi.drobnak@gmail.com>
2024-01-17 21:53:53 +0100

    refactor(calendar-helper): clean-up imports

--- src/app/modules/calendar-helper/calendar-helper.module.ts
+++ src/app/modules/calendar-helper/calendar-helper.module.ts
@@ -5,21 +5,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {
-  CalendarDateFormatter,
-  CalendarModule,
-  CalendarMomentDateFormatter,
-  DateAdapter,
-  MOMENT,
-} from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/moment';
-import * as moment from 'moment';
+import { CalendarModule } from 'angular-calendar';
 import { CalendarPaginatorComponent } from './components/calendar-paginator/calendar-paginator.component';
 import { CalendarViewSelectionComponent } from './components/calendar-view-selection/calendar-view-selection.component';
-
-export function momentAdapterFactory() {
-  return adapterFactory(moment);
-}
 
 @NgModule({
   declarations: [CalendarViewSelectionComponent, CalendarPaginatorComponent],
@@ -30,29 +18,8 @@ export function momentAdapterFactory() {
     MatButtonModule,
     MatProgressSpinnerModule,
     LayoutModule,
-    CalendarModule.forRoot(
-      {
-        provide: DateAdapter,
-        useFactory: momentAdapterFactory,
-      },
-      {
-        dateFormatter: {
-          provide: CalendarDateFormatter,
-          useClass: CalendarMomentDateFormatter,
-        },
-      }
-    ),
-  ],
-  providers: [
-    {
-      provide: MOMENT,
-      useValue: moment,
-    },
-  ],
-  exports: [
-    CalendarViewSelectionComponent,
-    CalendarPaginatorComponent,
     CalendarModule,
   ],
+  exports: [CalendarViewSelectionComponent, CalendarPaginatorComponent],
 })
 export class CalendarHelperModule {}
